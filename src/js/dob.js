cf12a71ff541d64b406c34139a93ff4c295c1998
Sharad Bhandari <67964044+SharadS28N@users.noreply.github.com>
2025-04-10 18:04:12 +0545

    Update dob.js

--- src/js/dob.js
+++ src/js/dob.js
@@ -1,21 +1,2 @@
-const fs = require('fs');
-
-// Your Date of Birth
-const dob = new Date('2000-04-10');
-
-// Calculate age
-const today = new Date();
-let age = today.getFullYear() - dob.getFullYear();
-const m = today.getMonth() - dob.getMonth();
-if (m < 0 || (m === 0 && today.getDate() < dob.getDate())) {
-  age--;
-}
-
-// Read the README
-let readme = fs.readFileSync('README.md', 'utf8');
-
-// Replace {{AGE}} with actual age
-readme = readme.replace(/{{AGE}}/g, age.toString());
-
-// Save updated README
-fs.writeFileSync('README.md', readme);
+const dob = '2006-03-11'; // Replace with your DOB
+module.exports = dob;
